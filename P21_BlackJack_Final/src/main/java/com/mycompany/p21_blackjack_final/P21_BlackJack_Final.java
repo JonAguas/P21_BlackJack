65297846a89e00cd147801fdb172abaf7506dfc0
alumno <alumno@eim-alu-83004.lab.unavarra.es>
2024-04-24 10:46:13 +0200

    este es nuestro proyecto

---
+++ P21_BlackJack_Final/src/main/java/com/mycompany/p21_blackjack_final/P21_BlackJack_Final.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p21_blackjack_final;
+
+/**
+ *
+ * @author alumno
+ */
+public class P21_BlackJack_Final {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
